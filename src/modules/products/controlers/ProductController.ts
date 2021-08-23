ae62547be7c1b97774ba58cdbc5810bd3e34fd8c
SilveiraTF <silveiraforneli@gmail.com>
2021-08-23 19:54:41 -0300

    versão 2.0

--- src/modules/products/controlers/ProductController.ts
+++ src/modules/products/controlers/ProductController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
+import { DeleteDateColumn } from "typeorm";
 import CreateProductService from "../services/CreateProductService";
+import DeleteProductService from "../services/DeleteProductService";
+import Product from "../typeorm/entities/Product";
 export default class ProductController{
     //cria o produto
     public async create(request: Request, response: Response): Promise<Response>{
@@ -9,5 +12,15 @@ export default class ProductController{
             name, price, quantity
         })
         return response.json(product);
+    }
+    //remove o produto
+    public async delete(request: Request, response: Response): Promise <Response> {
+        //recupera o id do produto
+        let {id} = request.params
+        //cria objeto
+        let deleteProduct = new DeleteProductService()
+        await deleteProduct.execute({id}); //efetivamente remove
+
+        return response.json([])
     }
 }
--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,3 +1,6 @@
+import AppError from "shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import ProductRepository from "../typeorm/repositories/ProductRepository"
 // vamos criar a interface de coleta de dados
 interface IRequest {
     id : string
@@ -6,5 +9,16 @@ interface IRequest {
 // criar classe
 class DeleteProductService{
     //cria o método execute
-    public async execute({id}: IRequest):Promise<void>
+    public async execute({id}: IRequest):Promise<void>{
+        //recupera o repositório do produto
+        let productRepository = getCustomRepository(ProductRepository);
+        //recupera o produto para remover
+        let product = await productRepository.findOne(id)
+        if (!product) {//produto não existe
+            throw new AppError('Produto não existe')
+        }
+        //produto existe
+        await productRepository.remove(product)
+    }
 }
+export default DeleteProductService;
