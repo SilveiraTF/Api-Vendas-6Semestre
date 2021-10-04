ebc9cac684fe83aae34163ef57c3feeec9127587
SilveiraTF <silveiraforneli@gmail.com>
2021-10-04 20:37:52 -0300

    versão 4.1

---
+++ src/modules/users/routes/user.routes.ts
@@ -0,0 +1,24 @@
+import { celebrate, Segments, Joi } from 'celebrate';
+import {Router} from 'express';
+
+
+import UserController from '../controlers/USerContoller';
+
+let userRouter = Router()
+
+let userController = new UserController()
+
+// não temos nada para validar
+userRouter.get('/', userController.index)
+// validar que o post precisa de um produto
+userRouter.post('/', 
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            email: Joi.string().required(),
+            password: Joi.string().required()
+        }
+    }),
+userController.create)
+
+export default userRouter
--- src/shared/http/routes.ts
+++ src/shared/http/routes.ts
@@ -4,8 +4,10 @@ const routes = Router();
 
 import productRouter from '../../modules/products/routes/product.routes'
 import alunoRouter from '../../modules/products/routes/aluno.routes'
+import userRouter from '../../modules/users/routes/User.routes'
 
 routes.use('/products', productRouter)
 routes.use('/alunos', alunoRouter)
+routes.use('/users', userRouter)
 
 export default routes
